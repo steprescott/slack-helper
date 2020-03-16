c8d4326218b3fac5e5f7e377a84af69e5596deea
Ste Prescott <ste.prescott@sky.uk>
2020-03-16 14:35:02 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import { WebClient, ChatPostMessageArguments } from '@slack/web-api';
+
+export class Slack {
+  private web: WebClient;
+
+  constructor(token: string) {
+    this.web = new WebClient(token);
+  }
+
+  async post(message: ChatPostMessageArguments) {
+    try {
+      await this.web.chat.postMessage(message);
+    } catch (error) {
+      return error;
+    }
+  }
+}
